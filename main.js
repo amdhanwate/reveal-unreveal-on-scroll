d980157815dd8fbea4a10b4c9b53d766929b43f1
coderAbhii <62416129+coderAbhii@users.noreply.github.com>
2021-07-06 23:09:40 +0530

    formatted code

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const body = document.body;
-        const header = document.querySelector("header");
-        const main = document.querySelector("main");
-        const headerHeight = document.querySelector("header").offsetHeight;
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const headerHeight = document.querySelector("header").offsetHeight;
 
-        main.style.top = headerHeight + "px";
+main.style.top = headerHeight + "px";
 
-        let lastScroll = 0;
+let lastScroll = 0;
 
-        window.addEventListener("scroll", () => {
-            let currentScroll = window.pageYOffset;
+window.addEventListener("scroll", () => {
+    let currentScroll = window.pageYOffset;
 
-            // console.log("current: ", currentScroll);
-            // console.log("last: ", lastScroll);
+    // console.log("current: ", currentScroll);
+    // console.log("last: ", lastScroll);
 
-            if (currentScroll - lastScroll > 0) {
-                // scrolled down -- header hide
-                header.classList.add("scroll-down");
-                header.classList.remove("scroll-up");
-            } else {
-                // scrolled up -- header show
-                header.classList.add("scroll-up");
-                header.classList.remove("scroll-down");
-            }
+    if (currentScroll - lastScroll > 0) {
+        // scrolled down -- header hide
+        header.classList.add("scroll-down");
+        header.classList.remove("scroll-up");
+    } else {
+        // scrolled up -- header show
+        header.classList.add("scroll-up");
+        header.classList.remove("scroll-down");
+    }
 
-            lastScroll = currentScroll;
-            // console.log("last: ", lastScroll);
+    lastScroll = currentScroll;
+    // console.log("last: ", lastScroll);
 
-        })
+})
